e047fd21dfc183fae82098269752a5740688081a
sab <sab@edgard.localdomain>
2022-05-25 13:05:00 -0300

    Create Cable struct

--- src/connectors/cable.rs
+++ src/connectors/cable.rs
@@ -1,1 +1,12 @@
-struct Cable {}
+use crate::base_gates::gate::Gate;
+
+#[derive(PartialEq)]
+struct Cable<'a> {
+    value: u64,
+    size: usize,
+    connected_inputs: Vec<(&'a mut Gate, Vec<usize>)>,
+    connected_output: &'a Gate
+}
+
+impl<'a> Cable<'a> {
+}
