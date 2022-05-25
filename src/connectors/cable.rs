373067ebad9a6f86957608d55fdeaa96add5174a
sab <sab@edgard.localdomain>
2022-05-25 18:40:17 -0300

    Change cable inputs from Vec to HashMap

--- src/connectors/cable.rs
+++ src/connectors/cable.rs
@@ -1,10 +1,11 @@
 use crate::base_gates::gate::Gate;
+use std::collections::HashMap;
 
 #[derive(PartialEq)]
 struct Cable<'a> {
     value: u64,
     size: usize,
-    connected_inputs: Vec<(&'a mut Gate, Vec<usize>)>,
+    connected_inputs: HashMap<&'a mut Gate, Vec<usize>>,
     connected_output: &'a Gate,
 }
 
