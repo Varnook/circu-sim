7ab20f8bb54b7c8dea18eb2376cb62c49da77030
sab <sab@edgard.localdomain>
2022-05-26 17:51:34 -0300

    Initial connect_input idea

--- src/connectors/cable.rs
+++ src/connectors/cable.rs
@@ -14,8 +14,16 @@ impl<'a> Cable<'a> {
     /// Connects a cable to a single gate's input.
     pub fn connect_input(mut self, gate_to_connect: &'a mut Gate, input_to_connect: usize) {
         match self.connected_inputs.get_mut(gate_to_connect) {
-            Some(gate_inputs) => if !gate_inputs.contains(&input_to_connect) { gate_inputs.push(input_to_connect) },
-            None => {self.connected_inputs.insert(gate_to_connect, vec![input_to_connect]); ()}
+            Some(gate_inputs) => {
+                if !gate_inputs.contains(&input_to_connect) {
+                    gate_inputs.push(input_to_connect)
+                }
+            }
+            None => {
+                self.connected_inputs
+                    .insert(gate_to_connect, vec![input_to_connect]);
+                ()
+            }
         }
     }
 }
