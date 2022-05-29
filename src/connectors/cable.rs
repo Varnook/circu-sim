349b7bb532c0d091786b706c5de8e6178e027c31
sab <sab@edgard.localdomain>
2022-05-29 17:50:47 -0300

    Add connect cable test

--- src/connectors/cable.rs
+++ src/connectors/cable.rs
@@ -1,18 +1,20 @@
 use crate::base_gates::gate::Gate;
+use crate::base_gates::gate_type::GateType;
 use std::collections::HashMap;
 
 #[derive(PartialEq)]
 struct Cable<'a> {
     value: u64,
     size: usize,
-    connected_inputs: HashMap<&'a mut Gate, Vec<usize>>,
+    connected_inputs: HashMap<&'a Gate, Vec<usize>>,
     connected_output: &'a Gate,
 }
 
 impl<'a> Cable<'a> {
-    // TODO Error handling when gate bus size and cable size differ.
     /// Connects a cable to a single gate's input.
     pub fn connect_input(mut self, gate_to_connect: &'a mut Gate, input_to_connect: usize) {
+        gate_to_connect.set_inputs(vec![(input_to_connect, self.value)]);
+
         match self.connected_inputs.get_mut(gate_to_connect) {
             Some(gate_inputs) => {
                 if !gate_inputs.contains(&input_to_connect) {
@@ -25,5 +27,24 @@ impl<'a> Cable<'a> {
                 ()
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn connect_gate() {
+        let mut g_in = Gate::new(GateType::Or, 2, 1).unwrap();
+        let g_out = Gate::new(GateType::Xor, 3, 1).unwrap();
+        let ca = Cable {
+            value: 1,
+            size: 1,
+            connected_inputs: HashMap::new(),
+            connected_output: &g_out,
+        };
+        assert_eq!(g_in.get_output(), 0);
+        ca.connect_input(&mut g_in, 0);
+        assert_eq!(g_in.get_output(), 1);
     }
 }
